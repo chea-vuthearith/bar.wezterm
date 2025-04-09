778064b221cdf8231357eb584530958914b1c13b
CheaVuthearith <cheavuthearith0@gmail.com>
2025-04-09 20:32:29 +0700

    fix: hardcoded plugin path

--- plugin/init.lua
+++ plugin/init.lua
@@ -18,8 +18,8 @@ end
 ---returns the name of the package, used when requiring modules
 ---@return string
 local function get_require_path()
-  local path = "httpssCssZssZsgithubsDscomsZsadriankarlensZsbarsDswezterm"
-  local path_trailing_slash = "httpssCssZssZsgithubsDscomsZsadriankarlensZsbarsDsweztermsZs"
+  local path = "httpssCssZssZsgithubsDscomsZschea-vuthearithsZsbarsDswezterm"
+  local path_trailing_slash = "httpssCssZssZsgithubsDscomsZschea-vuthearithsZsbarsDsweztermZs"
   return directory_exists(path_trailing_slash) and path_trailing_slash or path
 end
 
